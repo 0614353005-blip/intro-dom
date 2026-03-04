fe1cfa04635289fd6524003ab735b5cabbc09881
0614353005-blip <0614353005@s.tusd1.org>
2026-03-04 18:01:54 +0000

    done

--- script.js
+++ script.js
@@ -1,21 +1,25 @@
+
 console.log("Script started");
-
-
+// Variable track number of clicks
 let clicks = 0;
 
 // variable to store click display h1
 let clickDisplay = document.getElementById("click-display");
-``
+
 // set inital text
 clickDisplay.innerText = "Clicks: " + clicks;
 
 // callled when the potato is clicked
-function hanfClick() {
-    console.log("click");
+function handleClick() {
+    console.log("Click");
 
     // Add one to click count
     clicks = clicks + 1;
 
     // Update display
     clickDisplay.innerText = "Clicks: " + clicks;
+
+    if (clicks == 10) {
+        alert(" you have reached 10 clicks");
+    }
 }
